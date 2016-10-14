1beaff1027da63a2bb919d089d70cec237c77faa
Jason Prodonovich <prodonjs@gmail.com>
2016-10-14 08:13:13 -0400

    HW2

--- ch7/ex8_two_dimensional_lists.py
+++ ch7/ex8_two_dimensional_lists.py
@@ -9,22 +9,13 @@ student_scores = [
   [99, 89, 90]
 ]
 
-
-# To loop through a 2D list, we need to use two loops.
-# The first loop will iterate on the rows/students of the table.
-for r in range(len(student_scores)):
-  total = 0  # Let's keep a running total
-  # The second loop needs to look at the grade in each 'column'
-  # Notice how we need to say student_scores[r] to get the length
-  # of the list in position r of the student_scores list.
-  for c in range(len(student_scores[r])):
-    total += student_scores[r][c]
-  print('Student {} Average grade: {}'.format(r, total / 3))
-
 # Here's a slightly modified version that doesn't use indices
 # but rather just loops directly over the lists' items.
 for s in student_scores:
+  s.clear()
   total = 0  # Let's keep a running total
   for g in s:  # Remember, s is a list so we can iterate over it
     total += g
   print('Average grade: ', total / 3)
+
+print(student_scores)
